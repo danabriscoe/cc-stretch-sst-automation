c4d6842a81f190f25b9482f956eedb9c9ac92e5e
danabriscoe <danakbriscoe@gmail.com>
2023-07-09 17:32:48 +1200

    add daily pts

--- code/utils_parse_coords.R
+++ code/utils_parse_coords.R
@@ -4,6 +4,24 @@
 # see: https://docs.ropensci.org/parzer/reference/parse_lon_lat.html
 
 # then use output to update actual shipping locations
+make180 <- function(lon){
+  isnot360<-min(lon)<0
+  if (!isnot360) {
+    ind<-which(lon>180)
+    lon[ind]<-lon[ind]-360
+  }
+  return(lon)
+}
+make360 <- function(lon){
+  isnot360<-min(lon)<0
+  if(isnot360){
+    ind<-which(lon<0)
+    lon[ind]<-lon[ind]+360
+  }
+  return(lon)
+}
+
+
 
 lat = c("36° 59.0")
 long = c("145° 36.6")
@@ -17,5 +35,11 @@ parzer::parse_lon_lat(lon = "161° 55.1", lat = "41° 29.9")
 parzer::parse_lon_lat(lon = "170° 33.5", lat = "42° 50.8")
 parzer::parse_lon_lat(lon = "179° 44.1", lat = "43° 32.6")
 
-43°32.6N
-179°44.1E
+parzer::parse_lon_lat(lon = "170° 09.7", lat = "42° 49.9")
+
+make360(-170.1617)
+
+parzer::parse_lon_lat(lon = "161° 35.6", lat = "42° 11.0")
+# lon      lat
+# 161.5933 42.18333
+make360(-161.5933)
